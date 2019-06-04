b8b30345e2569356f13813dd351d0260397f1fe9
Jeremy Hamm <jeremythamm@gmail.com>
2019-06-04 08:49:23 -0700

    Update build process

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
+import Example from './Example.vue'
 
 new Vue({
-  render: h => h(App)
+  render: h => h(Example)
 }).$mount('#app')
