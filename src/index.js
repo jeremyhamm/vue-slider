0279367fed09f3a55a0c023618b890ffed622846
Jeremy Hamm <jeremythamm@gmail.com>
2019-06-06 08:04:10 -0700

    Update installation wrapper

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import Vue from "vue";
 import Slider from "@/components/Slider";
 
-Vue.component(Slider, Slider);
+// Export individual component
+export { Slider };
 
-export default Slider;
+// What should happen if the user installs the library as a plugin
+function install(Vue) {
+  Vue.component('Slider', Slider);
+}
+
+export default { install: install };
