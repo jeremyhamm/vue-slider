7ff9ad173953449f468eeec5732d5565e41fed93
Jeremy Hamm <jeremythamm@gmail.com>
2019-06-21 10:37:31 -0700

    Update css extract

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,7 @@ const path = require("path");
 
 module.exports = {
   lintOnSave: process.env.NODE_ENV !== 'production',
-  pluginOptions: {
-    "style-resources-loader": {
-      preProcessor: "scss",
-      patterns: [path.resolve(__dirname, "./src/assets/sass/variables.scss")]
-    }
+  css: { 
+    extract: false 
   }
 }
