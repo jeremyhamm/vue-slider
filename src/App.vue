b1a792ccad5fefab11942c8263edf68a7dd69c90
Jeremy Hamm <jeremythamm@gmail.com>
2019-06-21 15:29:33 -0700

    Revert files for demo

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <slider
+      :width="300"
+      format="push"
+      direction="left"
+      :opacity="0.15"
+      :links="[
+        {'id': 1, 'text': 'Link 1', 'url': 'https://github.com'},
+        {'id': 2, 'text': 'Link 2', 'url': 'https://github.com'},
+        {'id': 3, 'text': 'Link 3', 'url': 'https://github.com'}
+      ]"
+    >
+    </slider>
+  </div>
+</template>
+
+<script>
+import Slider from './components/Slider'
+export default {
+  name: 'app',
+  components: {
+    'slider': Slider
+  }
+}
+</script>
+
+<style lang="scss">
+  #app {
+    padding: 2em;
+  }
+</style>
