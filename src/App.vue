a859358ff168de01d063a546d4d57aa52a579ab6
Jeremy Hamm <jeremythamm@gmail.com>
2019-06-21 14:58:58 -0700

    Remove slider from app

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,5 @@
 <template>
   <div id="app">
-    <slider
-      :width="300"
-      format="push"
-      direction="left"
-      :opacity="0.15"
-      :links="[
-        {'id': 1, 'text': 'Link 1', 'url': 'https://github.com'},
-        {'id': 2, 'text': 'Link 2', 'url': 'https://github.com'},
-        {'id': 3, 'text': 'Link 3', 'url': 'https://github.com'}
-      ]"
-    >
-    </slider>
   </div>
 </template>
 
