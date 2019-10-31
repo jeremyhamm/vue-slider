22db5d4bb088291d04412417b3a1fcbd4b9066f3
Eliamar Tani <eliamartani@gmail.com>
2019-10-31 01:10:27 +0000

    Code refactoring. Added slot option.

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
     <!-- Nav Links -->
-    <div ref="slider" class="navMenu" :style="[menuDirection, menuWidth]">
-      <a href="javascript:void(0)" class="closebtn" @click="closeMenu()">&times;</a>
-      <a v-for="link in links" :key="link.id" :href="link.url">{{ link.text }}</a>
-    </div>
+    <nav class="navMenu" :style="[menuDirection, menuWidth]">
+      <a href="#" class="closebtn" @click.prevent="closeMenu">&times;</a>
+      <template v-if="links.length">
+        <a v-for="link in links" :key="link.id" :href="link.url">{{ link.text }}</a>
+      </template>
+      <slot v-else />
+    </nav>
     <!-- Hamburger Menu -->
-    <nav ref="menuIcon" class="navIcon" :style="iconDirection">
-      <a href="javascript:void(0)" @click="toggleMenu()" data-test-ref="navMenuLink">
+    <nav class="navIcon" :style="iconDirection">
+      <a href="#" @click.prevent="toggleMenu" data-test-ref="navMenuLink">
         <svg width="30" height="30">
           <path d="M0,5 30,5" :stroke="styles['menu-icon-color']" stroke-width="5"/>
           <path d="M0,14 30,14" :stroke="styles['menu-icon-color']" stroke-width="5"/>
@@ -32,12 +35,14 @@ export default {
     format: {
       type: String,
       required: false,
-      default: 'overlay'
+      default: 'overlay',
+      validator: (value) => ['push', 'full', 'overlay'].indexOf(value) > -1
     },
     direction: {
       type: String,
       required: false,
-      default: 'left'
+      default: 'left',
+      validator: (value) => ['left', 'right'].indexOf(value) > -1
     },
     opacity: {
       type: Number,
@@ -46,7 +51,8 @@ export default {
     },
     links: {
       type: Array,
-      required: true
+      required: false,
+      default: () => ([])
     }
   },
   data () {
@@ -59,41 +65,40 @@ export default {
   },
   computed: {
     menuDirection () {
-      return this.direction === 'right' ? { 'right': 0 } : { 'left': 0 }
+      return this.direction === 'right'
+        ? { 'right': 0 }
+        : { 'left': 0 }
     },
     iconDirection () {
-      return this.direction === 'right' ? { 'float': 'right' } : { 'float': 'left' }
+      return { 'float': this.direction }
     },
     app () {
       return document.getElementById('app')
     }
   },
+  mounted () {
+    if (!this.app) {
+      console.warn(`[Slider] The format was set to '${this.format}', but there is no element with an id 'app'. Add id='app' to the element to move.`)
+    }
+  },
   methods: {
     openMenu () {
-      if (this.opacity) {
-        document.body.style.backgroundColor = utilities.hexToRGB(styles['background-color'], this.opacity)
+      const width = `${this.width}px`
+      const marginDirection = `margin-${this.direction}`
+
+      this.menuWidth = {
+        'width': this.format === 'full'
+          ? '100%'
+          : width
       }
-      this.setFormat()
-    },
-    setFormat () {
-      const width = this.width.toString() + 'px'
-      if (this.format === 'overlay') {
-        this.menuWidth = { 'width': width }
-      } else if (this.format === 'full') {
-        this.menuWidth = { 'width': '100%' }
-      } else {
-        this.menuWidth = { 'width': width }
-        if (this.app) {
-          if (this.direction === 'right') {
-            this.app.style.marginRight = width
-            this.app.style.transition = 'margin-right .5s'
-          } else {
-            this.app.style.marginLeft = width
-            this.app.style.transition = 'margin-left .5s'
-          }
-        } else {
-          console.warn(`[Slider] The format was set to '${this.format}', but there is no element with an id 'app'. Add id='app' to the element to move.`)
-        }
+
+      if (this.opacity) {
+        document.body.style.background = utilities.hexToRGB(styles['background-color'], this.opacity)
+      }
+
+      if (this.app) {
+        this.app.style[marginDirection] = width
+        this.app.style.transition = `${marginDirection} .5s`
       }
     },
     closeMenu () {
@@ -103,7 +108,7 @@ export default {
         this.app.style.marginRight = '0'
       }
       if (this.opacity) {
-        document.body.style.backgroundColor = styles['background-color']
+        document.body.style.background = styles['background-color']
       }
     },
     toggleMenu () {
@@ -121,8 +126,8 @@ export default {
   @import '@/assets/sass/variables.scss';
 
   body {
-    background-color: $background-color;
-    transition: background-color .5s;
+    background: $background-color;
+    transition: background .5s;
   }
   #app {
     transition: margin-left .5s;
@@ -136,7 +141,7 @@ export default {
     position: fixed;
     z-index: 1;
     top: 0;
-    background-color: $menu-background;
+    background: $menu-background;
     overflow-x: hidden;
     padding-top: 60px;
     transition: 0.5s;
